5a5a96b754abbbd67309f27bfc98a8cb75b341b4
Manuel Lopera <114296815+LoperaUr@users.noreply.github.com>
2023-04-10 12:19:37 -0500

    Add files via upload

---
+++ src/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import vite from '../public/vite.svg'
+
+const Header = () => {
+  return (
+    <div>
+        <img src={vite} />
+				<nav>
+					<ul>
+						<li></li>
+						<li></li>
+						<li></li>
+					</ul>
+				</nav>
+    </div>
+  )
+}
+
+export default Header
