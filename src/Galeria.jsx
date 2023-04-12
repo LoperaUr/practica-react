6265d8b27cbb41a0717aa56577a383dc2272590f
Manuel Lopera <114296815+LoperaUr@users.noreply.github.com>
2023-04-12 13:07:00 -0500

    intento de galeria

---
+++ src/Galeria.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./galeria.css";
+
+const Galeria = ({ imagenes }) => {
+  return (
+    <div>
+      <h1>Galeria</h1>
+      <section className="container-galeria">
+        {imagenes.map((imagen) => (
+          <div className="item-galeria">
+            <h3>{imagen.nombre}</h3>
+            <img src={imagen.url} />
+          </div>
+        ))}
+      </section>
+    </div>
+  );
+};
+
+export default Galeria;
--- src/Principal.jsx
+++ src/Principal.jsx
@@ -1,10 +1,24 @@
 import React from "react";
+import Galeria from "./Galeria";
+
+import img1 from "./img/1.jpg";
+import img2 from "./img/2.jpg";
+import img3 from "./img/3.jpg";
+import img4 from "./img/4.jpg";
+
+const imagenes = [
+  { nombre: "imagen 1", url: img1 },
+  { nombre: "imagen 2", url: img2 },
+  { nombre: "imagen 3", url: img3 },
+  { nombre: "imagen 4", url: img4 },
+];
 
 const Principal = () => {
   return (
-  <div>
-    Principal
-  </div>);
+    <div>
+      <Galeria imagenes={imagenes} />
+    </div>
+  );
 };
 
 export default Principal;
